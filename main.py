758dc2737337d36737927a3c8c23ab3e30b204e4
jfrez <jonathan.frez@gmail.com>
2023-10-20 13:32:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,59 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from sklearn.metrics import mean_squared_error
+import datetime
+
+
 import streamlit as st
+
+
+
+model = keras.models.load_model('nn')
+
+st.title("Predictor de clientes")
+st.markdown("""
+Bienvenido, para continuar indique una fecha, marque si se trata de algun día especial o ambos
+
+
+""")
 cuando = st.date_input("Cuando:")
 especial = st.checkbox("Especial", value=False)
 feriado = st.checkbox("Feriado", value=False)
+
+fecha_inicio = cuando + datetime.timedelta(hours=10)
+fecha_fin = cuando + datetime.timedelta(hours=22)
+
+# Crear un rango de fechas y horas
+rango_horas = pd.date_range(start=fecha_inicio, end=fecha_fin, freq='H')
+
+# Crear un DataFrame
+df = pd.DataFrame({'fecha_hora': rango_horas})
+df["especial"] =especial  #OJO AQUI
+df["feriado"]=feriado
+
+# Preprocesamiento de datos
+df['fecha_hora'] = pd.to_datetime(df['fecha_hora'])
+df['year'] = df['fecha_hora'].dt.year
+df['month'] = df['fecha_hora'].dt.month
+df['hour'] = df['fecha_hora'].dt.hour
+df['day_of_week'] = df['fecha_hora'].dt.dayofweek
+df['day_of_month'] = df['fecha_hora'].dt.day
+df['day_of_year'] = df['fecha_hora'].dt.dayofyear
+
+# Agregar columna para la semana del mes
+df['week_of_month'] = (df['day_of_month'] - 1) // 7 + 1
+
+
+df = df.drop(["fecha_hora"],axis=1)  #Eliminar datos no numericos
+
+
 if st.button('Resultados: ' ,type="primary"):
-  st.write("Hola Mundo!")
+
+  data = tf.convert_to_tensor(df.to_numpy())
+  prediccion = model.predict(data)
+  df["prediccion"] = prediccion
+  st.dataframe(df) 
+
